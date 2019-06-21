7239b9018ce7193c8641c28f71620fc1f216002e
Dominique Oyco <dominickoyco@gmail.com>
2019-06-20 23:59:58 -0700

    finished part 7

--- Unity 3D Game/Assets/Scripts/GameEnding.cs
+++ Unity 3D Game/Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,35 +9,58 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
+    //a way to fade the canvas
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
 
+    //detects if the player has enter the ending field
     void OnTriggerEnter(Collider other)
     {
+        //if the player hits the victory collider
         if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
         }
     }
 
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
         }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
+
     }
 
-    void EndLevel()
+    //if player reaches the end, the exit background image will popup
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-
+        //game quits once the fade is finished
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
